44d1d143dd3fc85bf81d4b1605404082b78fdabe
Jasmine Xiong <66758256+xiongjasmine@users.noreply.github.com>
2021-09-18 13:58:33 -0400

    Update indeed-search.py

--- indeed-search.py
+++ indeed-search.py
@@ -1,6 +1,3 @@
-# This script was made following a YouTube tutorial from Izzy Analytics
-# Link to his GitHub: https://github.com/israel-dryer/Indeed-Job-Scraper
-
 import csv
 from datetime import datetime
 import requests
@@ -12,25 +9,43 @@ from bs4 import BeautifulSoup
 # gets an url based on position and location
 def get_url(position, location):
     template = "https://ca.indeed.com/jobs?q={}&l={}"
-    position = position.replace(' ', '+')
-    location = location.replace(' ', '+')
+    template = "https://ca.indeed.com/{}-jobs-in-{}"
+    position = position.replace(' ', '-')
+    location = location.replace(' ', '-')
     url = template.format(position, location)
     return url
 
 # extract job data from a single record
 def get_record(card):
     atag = card.h2.a
-    job_title = atag.get('title')
-    job_url = 'https://ca.indeed.com' + atag.get('href')
+    try:
+        job_title = card.find('h2', 'jobTitle').text
+        if job_title[0:3] == 'new':
+            job_title = job_title[3:]
+    except AttributeError:
+        job_title = ''
+    try:
+        job_url = 'https://ca.indeed.com' + card.get('href')
+    except AttributeError:
+        job_url = ''
+    try:
+        company = card.find('span', 'companyName').text
+    except AttributeError:
+        company = ''
+    try:
+        job_location = card.find('div', 'companyLocation').text
+    except AttributeError:
+        job_location = ''
+    try:
+        job_summary = card.find('div', 'job-snippet').text.strip()
+    except AttributeError:
+        job_summary = ''
+    try:
+        post_date = card.find('span', 'date').text
+    except AttributeError:
+        post_date = ''
 
-    company = card.find('span', 'company').text.strip()
-    job_location = card.find('div', 'recJobLoc').get('data-rc-loc')
-    job_summary = card.find('div', 'summary').text.strip()
-
-    post_date = card.find('span', 'date').text
-    today = datetime.today().strftime('%Y-%m-%d')
-
-    record = (job_title, company, job_location, post_date, today, job_summary, job_url)
+    record = (job_title, company, job_location, post_date, job_summary, job_url)
     return record
 
 # creates the list of job openings
@@ -41,7 +56,7 @@ def main(position, location):
     while True:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        cards = soup.find_all('div', 'jobsearch-SerpJobCard')
+        cards = soup.find_all('a', 'tapItem')
         for card in cards:
             record = get_record(card)
             records.append(record)
@@ -52,7 +67,7 @@ def main(position, location):
 
     with open('indeed-results.csv', 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
-        writer.writerow(['Job Title', 'Company', 'Location', 'Post Date', 'Extract Date', 'Summary', 'Job URL'])
+        writer.writerow(['Job Title', 'Company', 'Location', 'Posted Date', 'Summary', 'Job URL'])
         writer.writerows(records)
 
 # select the position and location desired
